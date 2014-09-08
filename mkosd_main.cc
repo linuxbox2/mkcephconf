012b3fb790513a2e1af4831b984fb4d385319dcf
Matt Benjamin <matt@linuxbox.com>
2014-09-08 12:45:17 -0500

    Add tmpfs, conf, and cluster proto modules.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- mkosd_main.cc
+++ mkosd_main.cc
@@ -1,10 +1,43 @@
-#include <iostream>
 
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+#include "prep_tmpfs.h"
+
+namespace po = boost::program_options;
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-  cout << argv[0] << endl;
+  try {
+    po::options_description desc("options");
+    string param_file;
+    string output_dir;
+
+    desc.add_options()
+      ("param_file",
+       po::value<string>(&param_file)->default_value("params.ini"),
+       "location of param ini file")
+      ("output_dir",
+       po::value<string>(&output_dir)->default_value("/tmp/mkosd_dir"),
+       "output directory")
+      ;
+
+    po::variables_map vm;        
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    cout << "param file: " << param_file << endl;
+    cout << "output dir: " << output_dir << endl;
+  }
+
+  catch(exception& e) {
+    cerr << __func__ << " error: " << e.what() << "\n";
+    return 1;
+  }
+  catch(...) {
+    cerr << __func__ << " unknown exception" << endl;
+  }
 
   return 0;
 }
